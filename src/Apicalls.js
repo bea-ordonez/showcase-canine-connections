418f98b9937e03dfe239523de067cbb1cf68ccec
Barbie Ordonez <ordonezbarbie@gmail.com>
2023-04-18 21:15:05 -0600

    Fetch dog details object and test displaying info on dom

---
+++ src/Apicalls.js
@@ -0,0 +1,15 @@
+async function fetchDogDetails() {
+
+  try {
+    const response = await fetch('https://dogapi.dog/api/v2/breeds');
+    if(!response.ok) {
+        throw new Error(response.status);
+    }
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+export default fetchDogDetails;
--- src/App/App.js
+++ src/App/App.js
@@ -1,11 +1,37 @@
+import { render } from '@testing-library/react';
+import React, {useEffect, useState} from 'react';
+import fetchDogDetails from '../Apicalls'; 
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <h2>hi</h2>
-    </div>
-  );
+  const [dogDetails, setDogDetails] = useState([]);
+
+  useEffect(() => {
+    async function getData() {
+      const response = await fetchDogDetails();
+      // console.log(response)
+      setDogDetails(response.data);
+    }
+    getData();
+  }, []);
+
+  function renderData() {
+    return dogDetails.map(obj =>
+      <div>
+        <h2>{obj.attributes.name}</h2>
+      </div>
+    )
+  }
+    
+  
+
+return (
+  <> 
+    <h2>Name: {renderData()}</h2>
+  </>
+
+)
+
 }
 
 export default App;
