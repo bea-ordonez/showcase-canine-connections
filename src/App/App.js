9f7000c1d22e6c0f11728a3b3b076988520e7c3b
Barbie Ordonez <ordonezbarbie@gmail.com>
2023-04-19 11:22:08 -0600

    Add function to fetch dog facts

--- src/App/App.js
+++ src/App/App.js
@@ -1,16 +1,17 @@
 import { render } from '@testing-library/react';
-import React, {useEffect, useState} from 'react';
-import fetchDogDetails from '../Apicalls'; 
+import React, { useEffect, useState } from 'react';
+import { fetchDogDetails, fetchDogFacts } from '../Apicalls'; 
 import { Header } from '../Header/Header'; 
 import './App.css';
 
 function App() {
   const [dogDetails, setDogDetails] = useState([]);
+  const [dogFacts, setDogFacts] = useState([]);
 
   useEffect(() => {
     async function getData() {
       const response = await fetchDogDetails();
-      // console.log(response)
+      console.log(response)
       setDogDetails(response.data);
     }
     getData();
@@ -23,11 +24,22 @@ function App() {
       </div>
     )
   }
+
+  useEffect(() => {
+    async function getData() {
+      const response = await fetchDogFacts();
+      console.log(response)
+      setDogFacts(response.data)
+    }
+    getData();
+  }, [])
+
+
   
 return (
   <main className='App'> 
     <Header />
-    <h2>Name: {renderData()}</h2>
+    {/* <h2>Name: {renderData()}</h2> */}
   </main>
 )}
 
