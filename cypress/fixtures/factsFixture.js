ea351684347f063201e1fdb7cb8ee225a1eeef25
Barbie Ordonez <ordonezbarbie@gmail.com>
2023-04-20 14:42:41 -0600

    Add cypress testing to main page and add fixtures for details and facts

---
+++ cypress/fixtures/factsFixture.js
@@ -0,0 +1,13 @@
+const factsFixture = {
+    "data":[
+        {
+            "id":"d8deefdd-0829-4fa5-bd23-989786cffb5a",
+            "type":"fact",
+            "attributes":
+            {"body":"A German Shepherd guide dog led her blind companion the entire 2,100 mile Appalachian Trail."
+            }
+        }
+    ]
+}
+
+export default factsFixture;
